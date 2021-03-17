a93177313b7123517da8505aaaf3afbea138b8a6
Nouha-06 <nouhaila.mellouki@gmail.com>
2021-03-17 21:16:59 +0100

    synchronisation

--- src/main/java/fr/univrouen/cv21rest/controllers/IndexController.java
+++ src/main/java/fr/univrouen/cv21rest/controllers/IndexController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-	
 	@GetMapping("/")
 	public String index() {
-	return "Hello cv21 !";
-	}
+		return"Hello cv21 !";
+		}
 
 }
