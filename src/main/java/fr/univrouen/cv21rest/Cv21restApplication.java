bf9872f4e0d9cac2bf54b8e5a7eb5e9aeacc1368
Nouha-06 <nouhaila.mellouki@gmail.com>
2021-03-17 19:24:41 +0100

    premier commit

---
+++ src/main/java/fr/univrouen/cv21rest/Cv21restApplication.java
@@ -0,0 +1,15 @@
+package fr.univrouen.cv21rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cv21restApplication {
+
+	public static void main(String[] args) {
+		System.getProperties().put("server.port", 8100);
+
+		SpringApplication.run(Cv21restApplication.class, args);
+	}
+
+}
